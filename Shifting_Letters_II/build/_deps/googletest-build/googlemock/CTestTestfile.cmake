4710b4ddda504a3776f4e13f70a13e37e6e09ab2
Borkanie <bogdanboboc1789@gmail.com>
2023-02-05 23:22:04 +0200

    Solves another challange

---
+++ Shifting_Letters_II/build/_deps/googletest-build/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /media/bogdan/SSD/LEETCODE/Shifting_Letters_II/build/_deps/googletest-src/googlemock
+# Build directory: /media/bogdan/SSD/LEETCODE/Shifting_Letters_II/build/_deps/googletest-build/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
